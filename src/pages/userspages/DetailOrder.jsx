ebff875d0fa4d1f27e3830a4634afbf6698918dc
Abi Wildan <muhamad11wildan4n@gmail.com>
2026-02-02 01:39:55 +0700

    feat: create header for detailorder

--- src/pages/userspages/DetailOrder.jsx
+++ src/pages/userspages/DetailOrder.jsx
@@ -1,8 +1,18 @@
+function HeaderTitle() {
+    return(
+        <div class="mb-8">
+            <h1 class="text-4xl font-bold text-black mb-1">Order #12354-09893</h1>
+            <p class="text-gray-400 text-sm">21 March 2023 at 10:30 AM</p>
+        </div>
+    )
+}
 
 export default function DetailOrder(){
     return (
-        <>
-            Detail Order
-        </>
+        <div  class="bg-gray-50 text-gray-800 p-6 md:p-12">
+            <div class="max-w-6xl mx-auto">
+                <HeaderTitle />
+            </div>
+        </div>
     )
 }
