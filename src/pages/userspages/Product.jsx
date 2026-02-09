4024d9068acb918998987e5e36a231fdc5d7aa95
AbiPasundan <muhamad11wildan4n@gmail.com>
2026-02-09 07:52:42 +0700

    feat: create reset filter func

--- src/pages/userspages/Product.jsx
+++ src/pages/userspages/Product.jsx
@@ -4,6 +4,8 @@ import { PiSlidersHorizontalBold } from "react-icons/pi";
 import { IoMdSearch } from "react-icons/io";
 
 import { FaCircleArrowLeft, FaCircleArrowRight } from "react-icons/fa6";
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 
 function ProductHero() {
@@ -30,39 +32,39 @@ function MobileFilter() {
     )
 }
 
-function ProductCarouselComp({img, title, text, bg = "bg-[#88B788]" }){
+function ProductCarouselComp({ img, title, text, bg = "bg-[#88B788]" }) {
     return (
         <>
-        <section className={`flex p-3 gap-5 ${bg} rounded-xl min-w-[400px]`}>
-            <div className='w-[30%]'>
-                <img src={img} alt="avatar" />
-            </div>
-            <div className='flex flex-col [&>p]:my-2 w-[70%] nowrap '>
-                <h2 className='font-bold'>{title}</h2>
-                <p className='font-normal'> {text} </p>
-                <span>klaim kupon</span>
-            </div>
-        </section>
-        <section className={`flex p-3 gap-5 ${bg} rounded-xl min-w-[400px]`}>
-            <div className='w-[30%]'>
-                <img src={img} alt="avatar" />
-            </div>
-            <div className='flex flex-col [&>p]:my-2 w-[70%] nowrap '>
-                <h2 className='font-bold'>{title}</h2>
-                <p className='font-normal'> {text} </p>
-                <span>klaim kupon</span>
-            </div>
-        </section>
-        <section className={`flex p-3 gap-5 ${bg} rounded-xl min-w-[400px]`}>
-            <div className='w-[30%]'>
-                <img src={img} alt="avatar" />
-            </div>
-            <div className='flex flex-col [&>p]:my-2 w-[70%] nowrap '>
-                <h2 className='font-bold'>{title}</h2>
-                <p className='font-normal'> {text} </p>
-                <span>klaim kupon</span>
-            </div>
-        </section>
+            <section className={`flex p-3 gap-5 ${bg} rounded-xl min-w-[400px]`}>
+                <div className='w-[30%]'>
+                    <img src={img} alt="avatar" />
+                </div>
+                <div className='flex flex-col [&>p]:my-2 w-[70%] nowrap '>
+                    <h2 className='font-bold'>{title}</h2>
+                    <p className='font-normal'> {text} </p>
+                    <span>klaim kupon</span>
+                </div>
+            </section>
+            <section className={`flex p-3 gap-5 ${bg} rounded-xl min-w-[400px]`}>
+                <div className='w-[30%]'>
+                    <img src={img} alt="avatar" />
+                </div>
+                <div className='flex flex-col [&>p]:my-2 w-[70%] nowrap '>
+                    <h2 className='font-bold'>{title}</h2>
+                    <p className='font-normal'> {text} </p>
+                    <span>klaim kupon</span>
+                </div>
+            </section>
+            <section className={`flex p-3 gap-5 ${bg} rounded-xl min-w-[400px]`}>
+                <div className='w-[30%]'>
+                    <img src={img} alt="avatar" />
+                </div>
+                <div className='flex flex-col [&>p]:my-2 w-[70%] nowrap '>
+                    <h2 className='font-bold'>{title}</h2>
+                    <p className='font-normal'> {text} </p>
+                    <span>klaim kupon</span>
+                </div>
+            </section>
         </>
 
     )
@@ -79,7 +81,7 @@ function ProductCarousel() {
                 </span>
             </div>
             <main className=" flex flex-col  justify-start overflow-x-auto gap-5 ">
-                <div className="mx-10 my-10 flex flex-row  justify-start gap-5  py-5"> 
+                <div className="mx-10 my-10 flex flex-row  justify-start gap-5  py-5">
                     <ProductCarouselComp img="/src/assets/img/userimg/women.png" title="Happy Mother Days" text="Get one of our menu for free" />
                     <ProductCarouselComp img="/src/assets/img/userimg/women.png" title="Happy Mother Days" text="Get one of our menu for free" />
                     <ProductCarouselComp img="/src/assets/img/userimg/women.png" title="Happy Mother Days" text="Get one of our menu for free" />
@@ -91,86 +93,109 @@ function ProductCarousel() {
 }
 
 function ProductFilter() {
+    const defaultValues = {
+        search: "",
+        category: [],
+        sort: [],
+        range: {}
+    }
+    const { register, handleSubmit, reset, } = useForm({defaultValues})
+    const onSubmit = e => {
+        console.log("berhas")
+        console.log(e)
+    }
+
     return (
-    <>
-        <section className='mx-10 rounded-xl sticky md:flex flex-col gap-5 p-5 bg-black text-white max-w-[30%] hidden '>
-            <header className='flex'>
-                <h4>Filter</h4>
-                <h4>Reset Filter</h4>
-            </header>
-            <div className='flex gap-3 flex-col'>
-                <span>Search</span>
-                <label htmlFor="search" className='p-3 border border-red-800 rounded-xl bg-white text-red-900 '>
-                    <input type="text" name="search" id="search" className='outline-none' placeholder='Search Your Product'/>
-                </label>
-            </div>
-            <div className='flex flex-col gap-10 [&>ul>h4]:text-[18px] [&>form>h4]:font-bold [&>form>label>input]:mr-5 [&>form>label>input]:accent-[#FF8906] '>
-                <form className='flex flex-col gap-2'>
-                    <h4>Category</h4>
-                    <label htmlFor="favorit">
-                        <input type="checkbox" name="favorit" id="favorit" />
-                        <span>Favorit Product</span>
+        <>
+            <form onSubmit={handleSubmit(onSubmit)} className='mx-10 rounded-xl sticky md:flex flex-col gap-5 p-5 bg-black text-white max-w-[30%] hidden '>
+                <header className='flex justify-between'>
+                    <h4>Filter</h4>
+                    <button onClick={() => reset({...defaultValues}) }>Reset Filter</button>
+                </header>
+                <div className='flex gap-3 flex-col'>
+                    <span>Search</span>
+                    <label htmlFor="search" className='p-3 border border-red-800 rounded-xl bg-white text-red-900 '>
+                        <input type="text" {...register("search")} id="search" className='outline-none' placeholder='Search Your Product' />
                     </label>
-                    <label htmlFor="noncoffe">
-                        <input type="checkbox" name="noncoffe" id="noncoffe" />
-                        <span>Non Coffe</span>
+                </div>
+                <div className='flex flex-col gap-10 [&>ul>h4]:text-[18px] [&>form>h4]:font-bold [&>form>label>input]:mr-5 [&>form>label>input]:accent-[#FF8906] '>
+                    <div className='flex flex-col gap-2'>
+                        <h4>Category</h4>
+                        <label htmlFor="favorit">
+                            <input type="checkbox" {...register("category")} value="favorite" id="favorit" />
+                            <span>Favorit Product</span>
+                        </label>
+                        <label htmlFor="noncoffe">
+                            <input type="checkbox" {...register("category")} value="coffe" id="noncoffe" />
+                            <span>Non Coffe</span>
+                        </label>
+                        <label htmlFor="coffee">
+                            <input type="checkbox" {...register("category")} value="foods" id="coffee" />
+                            <span>Coffe</span>
+                        </label>
+                        <label htmlFor="foods">
+                            <input type="checkbox" {...register("category")} value="favorit" id="foods" />
+                            <span>Foods</span>
+                        </label>
+                        <label htmlFor="addon">
+                            <input type="checkbox" {...register("category")} value="addon" id="addon" />
+                            <span>Add On</span>
+                        </label>
+                    </div>
+                    <div className='flex flex-col gap-2'>
+                        <h4>Sort By</h4>
+                        <label htmlFor="freeone">
+                            <input type="checkbox" {...register("sort")} value="freeone" id="freeone" />
+                            <span>Buy1 get1</span>
+                        </label>
+                        <label htmlFor="flashsale">
+                            <input type="checkbox" {...register("sort")} value="flashsale" id="flashsale" />
+                            <span>Flash Sale</span>
+                        </label>
+                        <label htmlFor="birthday">
+                            <input type="checkbox" {...register("sort")} value="birthday" id="birthday" />
+                            <span>BirthDay Package</span>
+                        </label>
+                        <label htmlFor="cheap">
+                            <input type="checkbox" {...register("sort")} value="cheap" id="cheap" />
+                            <span>Cheap</span>
+                        </label>
+                    </div>
+                </div>
+                <div className='flex gap-3 flex-col'>
+                    <span>Range Price</span>
+                    <label htmlFor="search" className='p-3 border border-red-800 rounded-xl bg-white text-red-900 '>
+                        <input type="range" name="rangeMin" id="rangeMin" className='outline-none' />
+                        <input type="range" name="rangeMax" id="rangeMax" className='outline-none' />
                     </label>
-                    <label htmlFor="coffee">
-                        <input type="checkbox" checked name="coffee" id="coffee" />
-                        <span>Coffe</span>
-                    </label>
-                    <label htmlFor="foods">
-                        <input type="checkbox" name="foods" id="foods" />
-                        <span>Foods</span>
-                    </label>
-                    <label htmlFor="addon">
-                        <input type="checkbox" name="addon" id="addon" />
-                        <span>Add On</span>
-                    </label>
-                </form>
-                <form className='flex flex-col gap-2'>
-                    <h4>Sort By</h4>
-                    <label htmlFor="freeone">
-                        <input type="checkbox" name="freeone" id="freeone" />
-                        <span>Buy1 get1</span>
-                    </label>
-                    <label htmlFor="flashsale">
-                        <input type="checkbox" checked name="flashsale" id="flashsale" />
-                        <span>Flash Sale</span>
-                    </label>
-                    <label htmlFor="birthday">
-                        <input type="checkbox" name="birthday" id="birthday" />
-                        <span>BirthDay Package</span>
-                    </label>
-                    <label htmlFor="cheap">
-                        <input type="checkbox" name="cheap" id="cheap" />
-                        <span>Cheap</span>
-                    </label>
-                </form>
-            </div>
-        </section>
-    </>
+                </div>
+                <div className='flex gap-3 flex-col'>
+                    <span>Range Price</span>
+                    <button type='submit' htmlFor="search" className='p-3 border border-red-800 rounded-xl bg-[#FF8906] text-black '>Apply Filter</button>
+                </div>
+            </form>
+        </>
     )
 }
 
 function MainProductFilter() {
     return (
         <>
-        <div className='flex flex-wrap gap-3'>
-            <ProductFilter />
-            <div className='flex flex-row flex-wrap gap-3 flex-1 justify-center'>
-                <ProductCard />
+            <div className='flex flex-wrap gap-3'>
+                <ProductFilter />
+                <div className='flex flex-row flex-wrap gap-3 flex-1 justify-center'>
+                    <ProductCard />
+                </div>
             </div>
-        </div>
-        <section className='flex flex-row justify-center items-center gap-5 my-10 [&>span]:py-[3px] [&>span]:px-[10px] [&>span]:bg-[#FF8906] [&>span]:rounded-full '>
-            <span>1</span><span>2</span><span>3</span><span>3</span><span><FaCircleArrowRight size={24} /></span>
-        </section>
+            <section className='flex flex-row justify-center items-center gap-5 my-10 [&>span]:py-[3px] [&>span]:px-[10px] [&>span]:bg-[#FF8906] [&>span]:rounded-full '>
+                <span>1</span><span>2</span><span>3</span><span>3</span><span><FaCircleArrowRight size={24} /></span>
+            </section>
         </>
     )
 }
 
 
-export default function Product(){
+export default function Product() {
     return (
         <>
             <ProductHero />
