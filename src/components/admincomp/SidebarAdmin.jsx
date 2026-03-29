0cc948d49d91e256f0fe71c63515a37aefa22ba4
AbiPasundan <muhamad11wildan4n@gmail.com>
2026-03-29 14:35:18 +0700

    src/

--- src/components/admincomp/SidebarAdmin.jsx
+++ src/components/admincomp/SidebarAdmin.jsx
@@ -4,56 +4,67 @@ import { BsPersonFill } from "react-icons/bs";
 import { IoBagOutline } from "react-icons/io5";
 import { CiLogout } from "react-icons/ci";
 import { Link, useNavigate } from "react-router";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { HamburgerContext } from "../hook/HamburgerContext";
 
-
-// import { HamburgerContext} from "../hook/HamburgerContext";
-
 export default function SidebarAdmin() {
-    const navigate = useNavigate()
-    const tokenAuth = localStorage.getItem("token_auth_admin");
-    if (!tokenAuth) {
-      navigate("/login");
-    }
-    
+    const navigate = useNavigate();
+    const { open } = useContext(HamburgerContext);
+
+    const getUserFromToken = () => {
+        const token = localStorage.getItem("token");
+        if (!token) return null;
+
+        try {
+            return JSON.parse(atob(token.split(".")[1]));
+        } catch {
+            return null;
+        }
+    };
+
+    const user = getUserFromToken();
+
+    useEffect(() => {
+        if (!user || user.role !== "admin") {
+            navigate("/");
+        }
+    }, [user, navigate]);
+
     const onClick = () => {
-        localStorage.removeItem("token_auth_admin");
-        navigate("/")
-    }
-    
-    const { open } = useContext(HamburgerContext)
+        localStorage.removeItem("token");
+        navigate("/");
+    };
 
     return (
         <>
             <aside id="aside" className={` ${open && '-translate-x-full'} fixed md:top-70 top-0 md:translate-x-0  bg-[#ffffff]`} >
-            <ul className="list-none [&>li]:px-5 [&>li]:mb-2 [&>li>a]:flex [&>li>a]:items-center [&>li>a]:gap-4 [&>li>a]:p-3 [&>li>a]:rounded-xl [&>li>a]:font-medium [&>li>a>img]:w-5">
-                <li>
-                <Link to="/admin" href="./dashboard.html" >
-                    <MdDashboard /> Dashboard
-                </Link>
-                </li>
-                <li>
-                <Link to="/admin/product" href="./product.html">
-                    <CiCoffeeCup />Product
-                </Link>
-                </li>
-                <li>
-                <Link to="/admin/order" href="./product.html">
-                    <IoBagOutline />Order
-                </Link>
-                </li>
-                <li>
-                <Link to="/admin/user" href="./product.html">
-                    <BsPersonFill />User
-                </Link>
-                </li>
-                <li onClick={onClick}>
-                    <Link href="#" >
-                        <CiLogout />Logout
-                    </Link>
-                </li>
-            </ul>
+                <ul className="list-none [&>li]:px-5 [&>li]:mb-2 [&>li>a]:flex [&>li>a]:items-center [&>li>a]:gap-4 [&>li>a]:p-3 [&>li>a]:rounded-xl [&>li>a]:font-medium [&>li>a>img]:w-5">
+                    <li>
+                        <Link to="/admin">
+                            <MdDashboard /> Dashboard
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/admin/product">
+                            <CiCoffeeCup />Product
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/admin/order">
+                            <IoBagOutline />Order
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/admin/user">
+                            <BsPersonFill />User
+                        </Link>
+                    </li>
+                    <li onClick={onClick}>
+                        <Link>
+                            <CiLogout />Logout
+                        </Link>
+                    </li>
+                </ul>
             </aside>
         </>
     )
