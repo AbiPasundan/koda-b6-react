6d5f651e01e9e0dc22a6ecb04c7b2f2037fe3e3c
AbiPasundan <muhamad11wildan4n@gmail.com>
2026-04-04 17:39:58 +0700

    fix: update ihstrory order seperate into component

--- src/pages/userspages/HistoryOrder.jsx
+++ src/pages/userspages/HistoryOrder.jsx
@@ -36,24 +36,13 @@ function FilterByDelivery({
     );
 }
 
+function OrderItems() {
 
-
-export default function HistoryOrder() {
     const orders = JSON.parse(localStorage.getItem("orders")) || [];
 
 
-    orders.forEach(e => {
-        console.log(e)
-        e.cart.forEach(j => {
-            console.log("ini j ciuh abcd")
-            console.log(j.pictures)
-        });
-    });
-
     const [currentPage, setCurrentPage] = useState(1);
-    const [statusFilter, setStatusFilter] = useState("On Progress");
-    const ordersPerPage = 10;
-
+    const ordersPerPage = 1;
     const indexOfLastOrder = currentPage * ordersPerPage;
     const indexOfFirstOrder = indexOfLastOrder - ordersPerPage;
 
@@ -64,6 +53,112 @@ export default function HistoryOrder() {
 
     const totalPages = Math.ceil(orders.length / ordersPerPage);
 
+    return (
+        <div className="grid grid-cols-1 lg:grid-cols-12 gap-8">
+            <div className="lg:col-span-7 space-y-4">
+                {currentOrders.map((order, i) => (
+                    <div key={i} className="bg-white p-5 mx-3 rounded-xl flex flex-col md:flex-row gap-6 items-start md:items-center">
+                        <div className="w-full md:w-24 h-24 hidden md:block ">
+                            <img loading="lazy" key={i} src={order.cart?.[0]?.product?.image?.[0]} alt="Coffee" className="w-full h-full object-cover" />
+                        </div>
+
+                        <div className="flex-1 grid grid-cols-2 md:grid-cols-4 gap-4 w-full">
+                            <div>
+                                <div className="flex items-center text-gray-400 text-xs mb-1 gap-1">
+                                    <PiNotepadBold />
+                                    No. Order
+                                </div>
+                                <div className="font-bold text-gray-900 text-sm">{order.no}</div>
+                                <Link to={`/detailorder/${order.no}`} state={{ order }} className="text-yellow-500 text-xs font-medium underline mt-1 block">Views Order Detail</Link>
+                            </div>
+                            <div>
+                                <div className="flex items-center text-gray-400 text-xs mb-1 gap-1">
+                                    <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
+                                    Date
+                                </div>
+                                <div className="font-bold text-gray-900 text-sm">{order.date}</div>
+                            </div>
+
+                            <div>
+                                <div className="flex items-center text-gray-400 text-xs mb-1 gap-1">
+                                    <VscArrowSwap />
+                                    Total
+                                </div>
+                                <div className="font-bold text-gray-900 text-sm">{order.total}</div>
+                            </div>
+
+                            <div>
+                                <div className="flex items-center text-gray-400 text-xs mb-1 gap-1">
+                                    <FaExpandArrowsAlt />
+                                    Status
+                                </div>
+                                <span className="inline-block bg-orange-100 text-orange-400 text-xs font-bold px-3 py-1 rounded-full">{order.status}</span>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+
+                <div className="flex justify-center items-center gap-2 my-10">
+                    <button
+                        disabled={currentPage === 1}
+                        onClick={() => setCurrentPage(prev => prev - 1)}
+                        className="w-10 h-10 bg-orange-500 text-white rounded-full flex items-center justify-center disabled:opacity-50"
+                    >
+                        <FaArrowRightLong className="rotate-180" />
+                    </button>
+
+                    {[...Array(totalPages)].map((_, index) => {
+                        const page = index + 1;
+
+                        return (
+                            <button
+                                key={page}
+                                onClick={() => setCurrentPage(page)}
+                                className={`w-10 h-10 rounded-full flex items-center justify-center font-bold text-sm transition
+                                                ${currentPage === page
+                                        ? "bg-orange-500 text-white"
+                                        : "bg-gray-200 text-gray-500 hover:bg-gray-300"
+                                    }`}
+                            >
+                                {page}
+                            </button>
+                        );
+                    })}
+                    <button
+                        disabled={currentPage === totalPages}
+                        onClick={() => setCurrentPage(prev => prev + 1)}
+                        className="w-10 h-10 bg-orange-500 text-white rounded-full flex items-center justify-center disabled:opacity-50"
+                    >
+                        <FaArrowRightLong />
+                    </button>
+                </div>
+            </div>
+            <div className="lg:col-span-5 mx-3">
+                <div className="bg-white p-8 rounded-xl border border-gray-100 sticky top-6">
+                    <div className="bg-black w-10 h-10 rounded-lg flex items-center justify-center mb-4">
+                        <CiMail color="red" />
+                    </div>
+
+                    <h3 className="font-bold text-lg mb-2">Send Us Message</h3>
+                    <p className="text-gray-500 text-sm leading-relaxed mb-6">
+                        If you unable to find answer or find your product quickly, please describe your problem and tell us. We will give you solution.
+                    </p>
+
+                    <a href="https://wa.me/6287753518802" target="_blank">
+                        <button className="w-full bg-orange-500 text-white font-semibold py-3 rounded-lg shadow-md hover:bg-orange-600 transition duration-200">
+                            Send Message
+                        </button>
+                    </a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default function HistoryOrder() {
+    const orders = JSON.parse(localStorage.getItem("orders")) || [];
+    const [statusFilter, setStatusFilter] = useState("On Progress");
+    const [currentPage, setCurrentPage] = useState(1);
 
     return (
         <>
@@ -86,104 +181,8 @@ export default function HistoryOrder() {
                     </div>
                 </div>
 
-                <div className="grid grid-cols-1 lg:grid-cols-12 gap-8">
-                    <div className="lg:col-span-7 space-y-4">
-                        {currentOrders.map((order, i) => (
-                            <div key={i} className="bg-white p-5 mx-3 rounded-xl flex flex-col md:flex-row gap-6 items-start md:items-center">
-                                <div className="w-full md:w-24 h-24 hidden md:block ">
-                                    <img loading="lazy" key={i} src={order.cart?.[0]?.product?.image?.[0]} alt="Coffee" className="w-full h-full object-cover" />
-                                </div>
+                <OrderItems />
 
-                                <div className="flex-1 grid grid-cols-2 md:grid-cols-4 gap-4 w-full">
-                                    <div>
-                                        <div className="flex items-center text-gray-400 text-xs mb-1 gap-1">
-                                            <PiNotepadBold />
-                                            No. Order
-                                        </div>
-                                        <div className="font-bold text-gray-900 text-sm">{order.no}</div>
-                                        <Link to={`/detailorder/${order.no}`} state={{ order }} className="text-yellow-500 text-xs font-medium underline mt-1 block">Views Order Detail</Link>
-                                    </div>
-                                    <div>
-                                        <div className="flex items-center text-gray-400 text-xs mb-1 gap-1">
-                                            <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
-                                            Date
-                                        </div>
-                                        <div className="font-bold text-gray-900 text-sm">{order.date}</div>
-                                    </div>
-
-                                    <div>
-                                        <div className="flex items-center text-gray-400 text-xs mb-1 gap-1">
-                                            <VscArrowSwap />
-                                            Total
-                                        </div>
-                                        <div className="font-bold text-gray-900 text-sm">{order.total}</div>
-                                    </div>
-
-                                    <div>
-                                        <div className="flex items-center text-gray-400 text-xs mb-1 gap-1">
-                                            <FaExpandArrowsAlt />
-                                            Status
-                                        </div>
-                                        <span className="inline-block bg-orange-100 text-orange-400 text-xs font-bold px-3 py-1 rounded-full">{order.status}</span>
-                                    </div>
-                                </div>
-                            </div>
-                        ))}
-
-                        <div className="flex justify-center items-center gap-2 my-10">
-                            <button
-                                disabled={currentPage === 1}
-                                onClick={() => setCurrentPage(prev => prev - 1)}
-                                className="w-10 h-10 bg-orange-500 text-white rounded-full flex items-center justify-center disabled:opacity-50"
-                            >
-                                <FaArrowRightLong className="rotate-180" />
-                            </button>
-
-                            {[...Array(totalPages)].map((_, index) => {
-                                const page = index + 1;
-
-                                return (
-                                    <button
-                                        key={page}
-                                        onClick={() => setCurrentPage(page)}
-                                        className={`w-10 h-10 rounded-full flex items-center justify-center font-bold text-sm transition
-                                                ${currentPage === page
-                                                ? "bg-orange-500 text-white"
-                                                : "bg-gray-200 text-gray-500 hover:bg-gray-300"
-                                            }`}
-                                    >
-                                        {page}
-                                    </button>
-                                );
-                            })}
-                            <button
-                                disabled={currentPage === totalPages}
-                                onClick={() => setCurrentPage(prev => prev + 1)}
-                                className="w-10 h-10 bg-orange-500 text-white rounded-full flex items-center justify-center disabled:opacity-50"
-                            >
-                                <FaArrowRightLong />
-                            </button>
-                        </div>
-                    </div>
-                    <div className="lg:col-span-5 mx-3">
-                        <div className="bg-white p-8 rounded-xl border border-gray-100 sticky top-6">
-                            <div className="bg-black w-10 h-10 rounded-lg flex items-center justify-center mb-4">
-                                <CiMail color="red" />
-                            </div>
-
-                            <h3 className="font-bold text-lg mb-2">Send Us Message</h3>
-                            <p className="text-gray-500 text-sm leading-relaxed mb-6">
-                                If you unable to find answer or find your product quickly, please describe your problem and tell us. We will give you solution.
-                            </p>
-
-                            <a href="https://wa.me/6287753518802" target="_blank">
-                                <button className="w-full bg-orange-500 text-white font-semibold py-3 rounded-lg shadow-md hover:bg-orange-600 transition duration-200">
-                                    Send Message
-                                </button>
-                            </a>
-                        </div>
-                    </div>
-                </div>
             </div>
         </>
     )
