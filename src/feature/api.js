6a6a504ee632a24e5cab1fb74f7b3754ec759ac4
AbiPasundan <muhamad11wildan4n@gmail.com>
2026-04-06 09:19:33 +0700

    chore change url

--- src/feature/api.js
+++ src/feature/api.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://wildan-backend.camps.fahrul.id",
+    baseUrl: "http://localhost:8888",
     prepareHeaders: (headers) => {
       const token = localStorage.getItem("token");
 
